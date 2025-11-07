7ce32f5b6d6ebdb928ca27dc416e8ef7a4bce970
muxueqz <zhangmingyuan240@gmail.com>
2025-11-07 16:34:29 +0800

    use type struct

--- i3_tools.nim
+++ i3_tools.nim
@@ -5,6 +5,8 @@ import os
 import osproc
 import tables
 import streams
+import std/[sequtils]
+import std/[options]
 
 
 const
@@ -16,11 +18,6 @@ type
     mtype*: int32
 const I3_IPC_MAGIC = "i3-ipc"
 
-# type
-#   # I3MessageType* {.pure.} = enum
-#   I3MessageType = enum
-#     command = 0, get_workspaces, subscribe, get_outputs = 3, get_tree = 4,
-#     get_marks, get_bar_config, get_version
 type
   I3MessageType* {.pure.} = enum
     command,
@@ -84,7 +81,6 @@ proc i3_msg(message_type: uint32, payload: string = ""): string =
   sock.close()
   return response_payload
 
-# proc ipc_query(req: uint32, msg: string = ""): JsonNode =
 proc ipc_query(req: I3MessageType, msg: string = ""): JsonNode =
   let ans = i3_msg(req.uint32, msg)
   return parseJson(ans)
@@ -114,56 +110,74 @@ proc switch_workspace(argv: seq[string]) =
   echo ipc_query(command, "workspace number " & newworkspace & ";")
   echo ipc_query(command, "move workspace to output " & active_display)
 
-proc extract_nodes_iterative(workspace: JsonNode): seq[JsonNode] =
+type
+  Node* = object
+    id*: int
+    name*: string
+    app_id*: Option[string]
+    focused*: bool
+    `type`*: string
+    nodes*: seq[Node]
+    floating_nodes*: seq[Node]
+    workspace*: Option[int]
+
+  TreeData* = object
+    nodes*: seq[Node]
+
+proc extract_windows*(workspace: Node): seq[Node] =
   result = @[]
 
-  proc process_node(node: JsonNode, workspace: JsonNode) =
-    node["workspace"] = %* workspace["id"].getInt
-    if node.getOrDefault("focused").getBool:
-      focused_workspace = workspace["id"].getInt
+  proc process_node(node: var Node) =
+    node.workspace = workspace.id.some()
+    if node.focused:
+      focused_workspace = workspace.id
 
-  let floating_nodes = workspace.getOrDefault("floating_nodes").getElems
-  for floating_node in floating_nodes:
-    process_node(floating_node, workspace)
-    result.add floating_node
-
-  var nodes = workspace.getOrDefault("nodes").getElems
-  var i = 0
-  while i < nodes.len:
-    let node = nodes[i]
-    i += 1
-    if node.getOrDefault("nodes").len == 0:
-      process_node(node, workspace)
+  var stack = concat(workspace.floating_nodes, workspace.nodes)
+  while stack.len > 0:
+    var node = stack.pop()
+    if concat(node.floating_nodes, node.nodes).len == 0:
+      process_node(node)
       result.add node
     else:
-      for inner_node in node["nodes"].getElems:
-        inner_node["workspace"] = %* workspace["id"].getInt  # Consistent with id
-        nodes.add inner_node
+      for child in concat(node.floating_nodes, node.nodes):
+        var c = child
+        c.workspace = workspace.id.some()
+        stack.add(c)
 
-# type
-#   Workspace = object
-#     id: int
-#     age: int
-#     `type`: Option[string]
+type
+  WindowInfo* = object
+    id*: int
+    name*: string
+    app_id*: string
+    focused*: bool
+    `type`*: string
+    nodes*: seq[WindowInfo]
 
-proc get_windows(only_workspace: bool = false): seq[JsonNode] =
-  let data = ipc_query(get_tree)
+  Workspace* = object
+    id*: int
+    name*: string
+    `type`*: string
+    nodes*: seq[WindowInfo]
 
-  var workspace_windows = initTable[int, seq[JsonNode]]()
-  for output in data["nodes"].getElems:
-    if output["type"].getStr == "output":
-      let workspaces = output["nodes"].getElems
-      for workspace in workspaces:
-        if workspace["type"].getStr == "workspace":
-          let workspace_id = workspace["id"].getInt
-          let windows = extract_nodes_iterative(workspace)
-          workspace_windows[workspace_id] = windows
+  Output* = object
+    name*: string
+    `type`*: string
+    nodes*: seq[Workspace]
 
+proc get_windows(only_workspace: bool = false): seq[Node] =
+  let data = ipc_query(get_tree).to(TreeData)
+
+  var workspace_windows = initTable[int, seq[Node]]()
   result = @[]
-  for k, v in workspace_windows:
-    if only_workspace and k != focused_workspace:
-      continue
-    result.add v
+  for output in data.nodes:
+    if output.`type` == "output":
+      for workspace in output.nodes:
+        if workspace.`type` == "workspace":
+          workspace_windows[workspace.id] = workspace.extract_windows
+  if only_workspace:
+    return workspace_windows[focused_workspace]
+  return workspace_windows.values.toSeq.concat
+
 
 proc select_window(argv: seq[string]) =
   var only_workspace = false
@@ -173,14 +187,11 @@ proc select_window(argv: seq[string]) =
   let r = get_windows(only_workspace)
   var dmenu_str = ""
   for n, i in r:
-    var icon = i.getOrDefault("app_id").getStr
-    # if icon == "":
-      # echo i
-      # icon = i.getOrDefault("window_properties", %*{})
+    var icon = i.app_id.get
+# if icon == "":
       # icon = i.getOrDefault("window_properties", %*{}).getOrDefault("class", %*"").getStr
     icon = app_id_mapping.getOrDefault(icon, icon)
-    let name = i["name"].getStr
-    dmenu_str.add icon & "\t" & name & "\0icon\x1f" & icon & "\n"
+    dmenu_str.add icon[0 ..< icon.len.min(18)] & "\t" & i.name      & "\0icon\x1f" & icon & "\n"
 
   let cmd = "fuzzel --dmenu --index -w 80 --counter --prompt \"Window > \""
   let (output, exitCode) = execCmdEx(cmd, input = dmenu_str)
@@ -190,7 +201,7 @@ proc select_window(argv: seq[string]) =
   if selected != "":
     let idx = parseInt(selected)
     let selected_window = r[idx]
-    let window_id = selected_window["id"].getInt
+    let window_id = selected_window.id
     discard execCmd("swaymsg [con_id=" & $window_id & "] focus")
 
 when isMainModule:
